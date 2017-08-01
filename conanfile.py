e62f976b6db8ff4a4094ce51728ce975be12497b
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:29:53 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,32 @@ class BoostRegexConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/regex"
-    description = "For a description of this library, please visit http://boost.org/regex "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "regex"
+    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Functional/1.64.0@bincrafters/testing", \
+                      "Boost.Integer/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Predef/1.64.0@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.64.0@bincrafters/testing",\
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing"
+
+                      #assert1 concept_check5 config0 core2 functional5 integer3 iterator5 mpl5 predef0 smart_ptr4 static_assert1 throw_exception2 type_traits3
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
