1083c6822a088ba451833bf4dd791859817df3a2
Jerry W <jerrywiltse@gmail.com>
2017-08-15 16:16:39 -0400

    mass update - change short_name to array - also add user_info for generator

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class BoostRegexConan(ConanFile):
     source_url = "https://github.com/boostorg/regex"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "regex"
+    lib_short_names = ["regex"]
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
     requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Concept_Check/1.64.0@bincrafters/testing", \
@@ -28,8 +28,9 @@ class BoostRegexConan(ConanFile):
                       #assert1 concept_check5 config0 core2 functional5 integer3 iterator5 mpl5 predef0 smart_ptr4 static_assert1 throw_exception2 type_traits3
 
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.source_url))
+        for lib_short_name in self.lib_short_names:
+            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+                     .format(self.version, lib_short_name)) 
 
     def build(self):
         boost_build = self.deps_cpp_info["Boost.Build"]
@@ -48,9 +49,12 @@ class BoostRegexConan(ConanFile):
         self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
 
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="include", src=include_dir)
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)		
+
         self.copy(pattern="*", dst="lib", src="stage/lib")
 
     def package_info(self):
+        self.user_info.lib_short_names = self.lib_short_names
         self.cpp_info.libs = self.collect_libs()
