bc22db13bb54572b7a842403edc88695d33a66e6
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:45:13 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ class BoostRegexConan(ConanFile):
     version = "1.64.0"
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-regex"
     source_url = "https://github.com/boostorg/regex"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -29,7 +30,7 @@ class BoostRegexConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def build(self):
