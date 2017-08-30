76b1e4b5bfb2474afa96065557b5ae711664a671
Rene Rivera <grafikrobot@gmail.com>
2017-08-30 14:23:52 -0500

    Fix deviation from regular package builds.

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,13 @@ class BoostRegexConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
+    short_paths = True
     url = "https://github.com/boostorg/regex"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "regex"
+    lib_short_names = ["regex"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
     requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Concept_Check/1.64.0@bincrafters/testing", \
@@ -27,30 +30,23 @@ class BoostRegexConan(ConanFile):
                       #assert1 concept_check5 config0 core2 functional5 integer3 iterator5 mpl5 predef0 smart_ptr4 static_assert1 throw_exception2 type_traits3
 
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
+        for lib_short_name in self.lib_short_names:
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def build(self):
-        boost_build = self.deps_cpp_info["Boost.Build"]
-        b2_bin_name = "b2.exe" if self.settings.os == "Windows" else "b2"
-        b2_bin_dir_name = boost_build.bindirs[0]
-        b2_full_path = os.path.join(boost_build.rootpath, b2_bin_dir_name, b2_bin_name)
-
-        toolsets = {
-          'gcc': 'gcc',
-          'Visual Studio': 'msvc',
-          'clang': 'clang',
-          'apple-clang': 'darwin'}
-
-        b2_toolset = toolsets[str(self.settings.compiler)]
-        
-        self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
+        self.run(self.deps_user_info['Boost.Generator'].b2_command)
 
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="include", src=include_dir)
-        lib_dir = os.path.join(self.build_folder, "stage/lib")
-        self.copy(pattern="*", dst="lib", src=lib_dir)
+        self.copy(pattern="*", dst="lib", src="stage/lib")
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)
 
     def package_info(self):
-        self.cpp_info.libs = ["boost_%s"%(self.lib_short_name)]
+        self.user_info.lib_short_names = ",".join(self.lib_short_names)
+        self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
