08374a044f4d56ac33a87b238e758a515aa9b366
Uilian Ries <uilianries@gmail.com>
2017-09-07 22:45:42 -0300

    Fix CI build
    - Added default Conan build template
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from conan.packager import ConanMultiPackager
 
+
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
-    builder.add_common_builds()
+    builder.add_common_builds(shared_option_name="Boost.Regex:shared")
     builder.run()
