c0ce8125c7d0722bb06bf873d887c4baebca6cc0
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:29:24 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -3,30 +3,30 @@ from conans import ConanFile, tools, os
 
 class BoostRegexConan(ConanFile):
     name = "Boost.Regex"
-    version = "1.64.0"
+    version = "1.65.1"
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-regex"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["regex"]
     options = {"shared": [True, False]}
     default_options = "shared=True"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
-    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Functional/1.64.0@bincrafters/testing", \
-                      "Boost.Integer/1.64.0@bincrafters/testing", \
-                      "Boost.Iterator/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Predef/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.64.0@bincrafters/testing",\
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing"    
+    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Concept_Check/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Functional/1.65.1@bincrafters/testing", \
+                      "Boost.Integer/1.65.1@bincrafters/testing", \
+                      "Boost.Iterator/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Predef/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing",\
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing"    
 
                       #assert1 concept_check5 config0 core2 functional5 integer3 iterator5 mpl5 predef0 smart_ptr4 static_assert1 throw_exception2 type_traits3
 
