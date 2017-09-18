1b9c3c1d8839aca521ef62e9427a88068fb9d4a7
Uilian Ries <uilianries@gmail.com>
2017-09-17 22:26:38 -0300

    Simplified test package
    - Replaced Catch test by Boost Regex example.
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,21 +1,9 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 #include <boost/regex.hpp>
+#include <string>
+#include <iostream>
 
-TEST_CASE( "Match the expression", "[regex]" ) {
-    const std::string string = "Boost Libraries";
-
-    boost::regex expression{"\\w+\\s\\w+"};
-    REQUIRE(boost::regex_match(string, expression));
-
-    boost::smatch what;
-    REQUIRE(boost::regex_search(string, what, expression));
-}
-
-TEST_CASE( "Replace string", "[regex]") {
-    std::string string = " Boost Libraries ";
-    boost::regex expression{"\\s"};
-    std::string fmt{"_"};
-    const std::string result = boost::regex_replace(string, expression, fmt);
-    REQUIRE(result == "_Boost_Libraries_");
+int main() {
+    const std::string s = "Boost Libraries";
+    boost::regex expr("\\w+\\s\\w+");
+    return !boost::regex_match(s, expr);
 }
