e3c60a044d3cfe0dd5bd92b7f9beaeb2a3cca7c9
Uilian Ries <uilianries@gmail.com>
2017-09-17 22:22:57 -0300

    Simplified test package
    - Removed Catch dependency and replaced by Boost Regex example.
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,16 +7,16 @@ class BoostRegexTestConan(ConanFile):
     generators = "cmake"
     username = os.getenv("CONAN_USERNAME", "bincrafters")
     channel = os.getenv("CONAN_CHANNEL", "testing")
-    requires = "Catch/1.9.6@uilianries/stable"
+    requires = "Boost.Regex/1.64.0@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
         cmake.build()
-        
+
     def imports(self):
         self.copy("*", dst="bin", src="lib")
-        
+
     def test(self):
         cmake = CMake(self)
         cmake.test()
